be8fca6d1fc9f1631794e51173ff63bc7c4c13cd
1 <zaidk311@gmail.com>
2015-12-24 21:34:59 +0100

    Solution to Exercise 4.10
    Signed-off-by: 1 <zaidk311@gmail.com>

--- CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/TreesAndGraphs/Exercise4_10.java
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/TreesAndGraphs/Exercise4_10.java
@@ -39,6 +39,67 @@ class CheckSubTree {
 			return (matchTree(T1.getLeft(), T2.getLeft()) && matchTree(T1.getRight(), T2.getRight()));
 		}
 	}
+
+	// Time complexity O(N + M) and Space complexity O(N + M)
+	public static boolean checkSubTree2(BinaryTreeNode<Integer> T1, BinaryTreeNode<Integer> T2) {
+		StringBuilder arrInOT1 = inOrderTraversal(T1);
+		StringBuilder arrInOT2 = inOrderTraversal(T2);
+		// System.out.println(arrInOT1 + "\n" + arrInOT2);
+
+		StringBuilder arrPreOT1 = preOrderTraversal(T1);
+		StringBuilder arrPreOT2 = preOrderTraversal(T2);
+		// System.out.println(arrPreOT1 + "\n" + arrPreOT2);
+
+		if (arrPreOT1 != null && arrPreOT2 != null) {
+			if (arrPreOT1.toString().contains(arrPreOT2.toString())) {
+				if (arrInOT1.toString().contains(arrInOT2.toString())) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public static StringBuilder inOrderTraversal(BinaryTreeNode<Integer> root) {
+		StringBuilder arr = new StringBuilder();
+		inOrder(root, arr);
+		return arr;
+	}
+
+	private static void inOrder(BinaryTreeNode<Integer> root, StringBuilder arr) {
+		if (root != null) {
+			inOrder(root.getLeft(), arr);
+			arr.append(root.getRoot());
+			inOrder(root.getRight(), arr);
+		} else {
+			// adding a sentinel value to distinguish between subTree that have
+			// duplicate values which can either on left or right side
+			arr.append(Integer.MIN_VALUE);
+		}
+	}
+
+	public static StringBuilder preOrderTraversal(BinaryTreeNode<Integer> root) {
+		StringBuilder arr = new StringBuilder();
+		preOrder(root, arr);
+		return arr;
+	}
+
+	private static void preOrder(BinaryTreeNode<Integer> root, StringBuilder arr) {
+		if (root != null) {
+			preOrder(root.getLeft(), arr);
+			preOrder(root.getRight(), arr);
+			arr.append(root.getRoot());
+		} else {
+			// adding a sentinel value to distinguish between subTree that have
+			// duplicate values which can either on left or right side
+			arr.append(Integer.MIN_VALUE);
+		}
+	}
 }
 
 public class Exercise4_10 {
@@ -65,13 +126,17 @@ public class Exercise4_10 {
 		e.setLeft(h);
 		e.setRight(i);
 		root.setRight(b);
-		System.out.println("B is subTree of Root: " + CheckSubTree.checkIfSubTree(root, b));
+
+		System.out.println("Solution 1: B is subTree of Root: " + CheckSubTree.checkIfSubTree(root, b));
+		System.out.println("Solution 2: B is subTree of Root: " + CheckSubTree.checkSubTree2(root, b));
 
 		BinaryTreeNode<Integer> aRoot = new BinaryTreeNode<Integer>(100);
 		aRoot.setLeft(new BinaryTreeNode<Integer>(90));
 		aRoot.setRight(new BinaryTreeNode<Integer>(110));
 
-		System.out.println("aRoot is subTree of Root: " + CheckSubTree.checkIfSubTree(root, aRoot));
+		System.out.println("Solution 1: aRoot is subTree of Root: " + CheckSubTree.checkIfSubTree(root, aRoot));
+		System.out.println("Solution 2: aRoot is subTree of Root: " + CheckSubTree.checkSubTree2(root, aRoot));
+
 	}
 
 }
