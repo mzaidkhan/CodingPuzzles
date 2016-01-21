492132670f2da5cb00353b3f52c8cf2add54c1f9
1 <zaidk311@gmail.com>
2016-01-21 23:43:34 +0100

    Adding top down recursive solution.
    Signed-off-by: 1 <zaidk311@gmail.com>

--- CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_2.java
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_2.java
@@ -85,8 +85,9 @@ public class Exercise8_2 {
 		}
 	}
 
-	public void clearPath() {
+	public void clearCaches() {
 		this.path.clear();
+		this.map.clear();
 	}
 
 	public int getDestinationRow() {
@@ -99,6 +100,10 @@ public class Exercise8_2 {
 
 	public boolean atOrigin(int row, int col) {
 		return (originRow == row && originCol == col);
+	}
+
+	public boolean atDestination(int row, int col) {
+		return (getDestinationRow() == row && getDestinationCol() == col);
 	}
 
 	// This solution takes 0(2^(row+col)) time and space
@@ -137,6 +142,28 @@ public class Exercise8_2 {
 
 	}
 
+	public boolean pathFinderTopDown(int row, int col) {
+		if (row > getDestinationCol() || col > getDestinationRow()) {
+			return false;
+		}
+		if (!maze[row][col]) {
+			return false;
+		}
+
+		Point currPos = new Point(row, col);
+		if (map.containsKey(currPos)) {
+			return map.get(currPos);
+		}
+
+		boolean success = false;
+		if (atDestination(row, col) || pathFinderTopDown(row + 1, col) || pathFinderTopDown(row, col + 1)) {
+			path.add(currPos);
+			success = true;
+		}
+		map.put(currPos, success);
+		return success;
+	}
+
 	public static void main(String[] args) {
 
 		Exercise8_2 pathFinder = new Exercise8_2();
@@ -150,18 +177,27 @@ public class Exercise8_2 {
 			System.out.println("No solution.");
 		}
 
-		pathFinder.clearPath();
 		pathFinder.findPathMemoization(pathFinder.getDestinationRow(), pathFinder.getDestinationCol());
 		System.out.println("\nResult of O((row*col)) solution: ");
 		if (pathFinder.path.size() > 0) {
 			for (Point point : pathFinder.path) {
 				System.out.println(point);
 			}
+			pathFinder.printMaze();
 		} else {
 			System.out.println("No solution.");
 		}
+		pathFinder.clearCaches();
 
-		pathFinder.printMaze();
+		pathFinder.pathFinderTopDown(pathFinder.originRow, pathFinder.originCol);
+		System.out.println("\nResult of Top Down solution solution: ");
+		if (pathFinder.path.size() > 0) {
+			for (Point point : pathFinder.path) {
+				System.out.println(point);
+			}
+			pathFinder.printMaze();
+		} else {
+			System.out.println("No solution.");
+		}
 	}
-
 }
