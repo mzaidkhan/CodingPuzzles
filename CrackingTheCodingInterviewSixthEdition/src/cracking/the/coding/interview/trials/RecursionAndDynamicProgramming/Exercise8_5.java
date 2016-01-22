d622d686f7be02d16ef62929f0f15445f9160ccf
1 <zaidk311@gmail.com>
2016-01-22 22:29:16 +0100

    Brute force recursive solution for now.
    Signed-off-by: 1 <zaidk311@gmail.com>

---
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_5.java
@@ -0,0 +1,33 @@
+package cracking.the.coding.interview.trials.RecursionAndDynamicProgramming;
+
+public class Exercise8_5 {
+
+	// Brute force solution.
+	// Time complexity O(B), where B is the value of value
+	public static int recMultiply(int a, int b) {
+		if (b < 0 && a < 0) {
+			a = -a;
+			b = -b;
+		} else if (b < 0) {
+			int c = a;
+			a = b;
+			b = c;
+		}
+		return recMulti(a, b);
+	}
+
+	private static int recMulti(int a, int b) {
+		if (b < 0) {
+			return 0;
+		} else if (b == 0) {
+			return a;
+		}
+		return a + recMultiply(a, --b);
+	}
+
+	public static void main(String[] args) {
+		int a = 11, b = -12;
+		System.out.println("Brute force solution " + a + " x " + b + " = " + recMultiply(a, b));
+	}
+
+}
