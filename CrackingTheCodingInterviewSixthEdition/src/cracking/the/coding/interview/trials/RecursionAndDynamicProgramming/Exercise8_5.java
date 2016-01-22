689f8cca1d6c8c77903f6cddd97375cc9863d625
1 <zaidk311@gmail.com>
2016-01-22 22:40:27 +0100

    ;)
    Signed-off-by: 1 <zaidk311@gmail.com>

--- CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_5.java
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_5.java
@@ -5,7 +5,7 @@ public class Exercise8_5 {
 	// Brute force solution.
 	// Time complexity O(B), where B is the value of value
 	public static int recMultiply(int a, int b) {
-		if (b == 0) {
+		if (b == 0 || a == 0) {
 			return 0;
 		} else if (b < 0 && a < 0) {
 			a = -a;
@@ -28,7 +28,7 @@ public class Exercise8_5 {
 	}
 
 	public static void main(String[] args) {
-		int a = 12, b = -3;
+		int a = 1, b = -12;
 		System.out.println("Brute force solution " + a + " x " + b + " = " + recMultiply(a, b));
 	}
 
