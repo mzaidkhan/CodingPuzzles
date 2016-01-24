3180ba89ac4dd71d75f8165aeb7297a06a440d9a
1 <zaidk311@gmail.com>
2016-01-24 01:17:14 +0100

    Solution to Exercise 8.5
    Signed-off-by: 1 <zaidk311@gmail.com>

--- CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_5.java
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_5.java
@@ -21,15 +21,58 @@ public class Exercise8_5 {
 	private static int recMulti(int a, int b) {
 		if (b < 0) {
 			return 0;
-		} else if (b == 0) {
-			return a;
 		}
 		return a + recMultiply(a, --b);
 	}
 
+	// Divide and conquer method. Time complexity 0(Log of smaller number)
+	public static int multiply(int a, int b) {
+		int captureSign = 0;
+		if (b == 0 || a == 0) {
+			return 0;
+		} else if (b < 0 && a < 0) {
+			a = -a;
+			b = -b;
+		} else if (b < a) {
+			int c = a;
+			a = b;
+			b = c;
+		}
+
+		if (a < 0) {
+			captureSign = a;
+			a = -a;
+		}
+
+		int result = multiplyRec(a, b);
+		if (captureSign < 0) {
+			result = -result;
+		}
+		return result;
+	}
+
+	private static int multiplyRec(int a, int b) {
+		if (a == 0) {
+			return 0;
+		} else if (a == 1) {
+			return b;
+		}
+		int half = a >> 1;
+
+		int res = multiplyRec(half, b);
+
+		if (a % 2 == 0) {
+			return res + res;
+		} else {
+			return res + res + b;
+		}
+	}
+
 	public static void main(String[] args) {
-		int a = 1, b = -12;
+		int a = 12, b = -11;
 		System.out.println("Brute force solution " + a + " x " + b + " = " + recMultiply(a, b));
+		System.out.println("Divide and conquer solution " + a + " x " + b + " = " + multiply(a, b));
+
 	}
 
 }
