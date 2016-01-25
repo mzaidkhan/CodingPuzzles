b322d30b7b4352ff30dfcbe2623588e6068c793f
1 <zaidk311@gmail.com>
2016-01-25 21:43:26 +0100

    Optimized function calls: For 1st Call: Only one possible unique combination possible; that is (). For 2nd Call: Only two possible unique combinations possible; they are ()() and (()). All other Calls: we can go ahead with the three regularly calls.
    Signed-off-by: 1 <zaidk311@gmail.com>

--- CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_9.java
+++ CrackingTheCodingInterviewSixthEdition/src/cracking/the/coding/interview/trials/RecursionAndDynamicProgramming/Exercise8_9.java
@@ -4,12 +4,16 @@ import java.util.HashSet;
 
 public class Exercise8_9 {
 
+	public static boolean printEnabled = true;
+
 	// First instinct brute force solution using HashSet to eliminate duplicates
 	// (very slow :))
 	public static void genParens(int remaining, int lenOfComb, String prefix, HashSet<String> hSet) {
 		if (prefix.length() == lenOfComb) {
 			if (hSet.add(prefix)) {
-				System.out.println(prefix);
+				if (!printEnabled) {
+					System.out.println(prefix);
+				}
 			}
 		} else {
 			for (int i = 1; i <= remaining; i++) {
@@ -20,10 +24,53 @@ public class Exercise8_9 {
 		}
 	}
 
+	// Optimized code:
+	// For 1st Call: Only one possible unique combination possible that is ()
+	// For 2nd Call: Only two possible unique combinations possible they are
+	// ()() and (())
+	// All other Calls: we can go ahead with the three regularly calls
+	public static void genParens2(int remaining, int lenOfComb, String prefix, HashSet<String> hSet) {
+		if (prefix.length() == lenOfComb) {
+			if (hSet.add(prefix)) {
+				if (!printEnabled) {
+					System.out.println(prefix);
+				}
+			}
+		} else {
+			// First call
+			if (remaining == lenOfComb / 2) {
+				genParens(remaining - 1, lenOfComb, prefix + "()", hSet);
+			}
+			// Second call
+			else if (remaining + 1 == lenOfComb / 2) {
+				genParens(remaining - 1, lenOfComb, prefix + "()", hSet);
+				genParens(remaining - 1, lenOfComb, "(" + prefix + ")", hSet);
+			}
+			// Subsequent calls
+			else {
+				genParens(remaining - 1, lenOfComb, prefix + "()", hSet);
+				genParens(remaining - 1, lenOfComb, "(" + prefix + ")", hSet);
+				genParens(remaining - 1, lenOfComb, "()" + prefix, hSet);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		HashSet<String> hSet = new HashSet<String>();
-		int noOfParens = 3;
+		int noOfParens = 10;
 		genParens(noOfParens, noOfParens * 2, "", hSet);
+		System.out.println("(Slow version) Total no of possible combinations " + hSet.size());
+		HashSet<String> hSet2 = new HashSet<String>();
+		genParens2(noOfParens, noOfParens * 2, "", hSet2);
+		System.out.println("(A bit better version) Total no of possible combinations " + hSet.size());
+
+		if (hSet.containsAll(hSet2)) {
+			System.out.println("Test Passed: Same number of combinations in both solutions");
+		} else {
+			System.err.println("Test Failed: Different number of combinations in both solutions");
+
+		}
+
 	}
 
 }
